8308d66f51c30ab4babe24f8e3a71d40a7a7f430
Emin Khateeb <emin@emin.ch>
2019-10-19 00:01:54 +0200

    Updates

---
+++ precache-manifest.d606ca12084b19e5e21e279b7d7ebdac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebce77fa1c85fb758fa0c12cb2418ab6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d94136bc1f8a6e9aef5e",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ff63781dd4519bdbcf8f",
+    "url": "/static/css/main.83939651.chunk.css"
+  },
+  {
+    "revision": "d94136bc1f8a6e9aef5e",
+    "url": "/static/js/2.75d57857.chunk.js"
+  },
+  {
+    "revision": "ff63781dd4519bdbcf8f",
+    "url": "/static/js/main.7dc45021.chunk.js"
+  },
+  {
+    "revision": "fb222bd84a62e374b162",
+    "url": "/static/js/runtime-main.0c8b2a69.js"
+  }
+]);
