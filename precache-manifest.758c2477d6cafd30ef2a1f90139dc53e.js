e082b5196c54c8c756f7bfc305b16d3f8c991e98
Emin Khateeb <emin@emin.ch>
2019-10-19 11:17:30 +0200

    Updates

---
+++ precache-manifest.758c2477d6cafd30ef2a1f90139dc53e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a35182983a9310aa386ea7cf5a71262f",
+    "url": "/overtone-reactor/index.html"
+  },
+  {
+    "revision": "e48faec3b77a841363b7",
+    "url": "/overtone-reactor/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "2f865e0be4b38289cbef",
+    "url": "/overtone-reactor/static/css/main.3ef65675.chunk.css"
+  },
+  {
+    "revision": "e48faec3b77a841363b7",
+    "url": "/overtone-reactor/static/js/2.269aaa9f.chunk.js"
+  },
+  {
+    "revision": "2f865e0be4b38289cbef",
+    "url": "/overtone-reactor/static/js/main.422b223c.chunk.js"
+  },
+  {
+    "revision": "12e47d9c05a86c9b489d",
+    "url": "/overtone-reactor/static/js/runtime-main.2b5c7667.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/overtone-reactor/precache-manifest.c7dea3bb4a913e27b864eb5f98cfbb42.js"
+  "/overtone-reactor/precache-manifest.758c2477d6cafd30ef2a1f90139dc53e.js"
 );
 
 self.addEventListener('message', (event) => {
